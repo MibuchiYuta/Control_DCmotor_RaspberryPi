1763a5b6345845fc2c07892db028ead49e8351c3
mibuchiyuta <6343yuuta@gmail.com>
2020-12-01 09:53:01 +0000

    update

--- myled.c
+++ myled.c
@@ -3,21 +3,28 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
+#include <linux/io.h>
+
 MODULE_AUTHOR("Mibuchi Yuta and Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for full color LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
+
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
+static volatile u32 *gpio_base = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 	char c;   //読み込んだ字を入れる変数
 	if(copy_from_user(&c,buf,sizeof(char)))
 	return -EFAULT;
+	if(c == '0')
+		gpio_base[10] = 1 << 25;
+	else if(c == '1')
+		gpio_base[7] = 1 << 25;
 	
-	printk(KERN_INFO "receive %c\n",c);
 	return 1;
 }
 static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
@@ -60,6 +67,16 @@ static int __init init_mod(void)
                 return PTR_ERR(cls);
 	}
 	device_create(cls, NULL, dev, NULL, "myled%d",MINOR(dev));
+
+	gpio_base = ioremap_nocache(0x3f200000, 0xA0);
+	
+	const u32 led = 25;
+	const u32 index = led/10;
+	const u32 shift = (led%10)*3;
+	const u32 mask = ~(0x7 << shift);
+	
+	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+	
 	return 0;
 }
 
