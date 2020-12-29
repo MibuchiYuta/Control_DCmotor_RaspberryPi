682b8b9241fd05a45ef12e09bfa4ef4e545098fc
mibuchiyuta <6343yuuta@gmail.com>
2020-12-29 14:27:26 +0000

    update

--- motor.c
+++ motor.c
@@ -23,25 +23,22 @@ static ssize_t motor_write(struct file* filp, const char* buf, size_t count, lof
 	char c;
 	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
-	if(c != '\n'){	
-		if(c == '1'){
-			gpio_base[7] = 1 << 25;
-			gpio_base[10] = 1 << 24;
-			msleep(1000);
-			gpio_base[10]= 1 << 25;
-			}
-
-		else if(c == '2'){
-			gpio_base[7] = 1 << 24;
-			gpio_base[10] = 1 << 25;
-			msleep(1000);
-			gpio_base[10]= 1 << 24;
-			}
-		else if(c == '0'){
-			gpio_base[10] = 1 << 24;
-			gpio_base[10] = 1 << 25;
+	if(c == '+'){
+		gpio_base[7] = 1 << 25;
+		gpio_base[10] = 1 << 24;
+		msleep(500);
+		gpio_base[10] = 1 << 25;
+		gpio_base[7] = 1 << 24;
+		msleep(200);
+		gpio_base[10] = 1 << 24;
 		}
-	}
+
+	else if(c == '-'){
+		gpio_base[7] = 1 << 24;
+		gpio_base[10] = 1 << 25;
+		msleep(1000);
+		gpio_base[10]= 1 << 24;
+		}
 	return 1;
 }
 
